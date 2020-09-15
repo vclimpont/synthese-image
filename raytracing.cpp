35573950a6942a2b7aa723203f7b83bf67f4ebad
Valentin CLIMPONT <valentin.climpont@gmail.com>
2020-09-15 12:04:45 +0200

    change pixel value according to intersection with a sphere

--- raytracing.cpp
+++ raytracing.cpp
@@ -44,26 +44,38 @@ void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsi
 
 int main()
 {
+    Vector3 dirPoint = Vector3(0, 0, 0);
+    Vector3 dirRay = Vector3(0, 0, 1);
+    Vector3 center = Vector3(250, 250, 50);
+    float radius = 100.0f;
+
     const char* filename = "test.png";
 
     //generate some image
     unsigned width = 512, height = 512;
     std::vector<unsigned char> image;
     image.resize(width * height * 4);
+
     for (unsigned y = 0; y < height; y++)
         for (unsigned x = 0; x < width; x++) {
-            image[4 * width * y + 4 * x + 0] = 255;
-            image[4 * width * y + 4 * x + 1] = 255;
-            image[4 * width * y + 4 * x + 2] = 255;
-            image[4 * width * y + 4 * x + 3] = 255;
+            
+            float n = hit_sphere(Vector3(x, y, 0), dirRay, center, radius);
+
+            if (n >= 0)
+            {
+                image[4 * width * y + 4 * x + 0] = n;
+                image[4 * width * y + 4 * x + 1] = n;
+                image[4 * width * y + 4 * x + 2] = n;
+                image[4 * width * y + 4 * x + 3] = n;
+            }
+            else
+            {
+                image[4 * width * y + 4 * x + 0] = 255;
+                image[4 * width * y + 4 * x + 1] = 0;
+                image[4 * width * y + 4 * x + 2] = 0;
+                image[4 * width * y + 4 * x + 3] = 255;
+            }
         }
 
     encodeOneStep(filename, image, width, height);
-
-    Vector3 dirPoint = Vector3(0, 0, 0);
-    Vector3 dirRay = Vector3(0, 0, 1);
-    Vector3 center = Vector3(0, 0, 10);
-    float radius = 5.0f;
-
-    std::cout << hit_sphere(dirPoint, dirRay, center, radius);
 }
